4c753857213212a09b6604db28c8702abb66f60f
Arnfinn <arnfinn.steindal@uit.no>
2013-04-24 12:31:22 +0000

    debug SHG

--- SHG.py
+++ SHG.py
@@ -96,13 +96,13 @@ parser.add_argument("-i", "--input",dest="filename", nargs="+",
 parser.add_argument("-d", "--dipole",action="store_true",default=False, dest="dipole", 
                     help="Compute the SHG (beta||) in the direction of the permanent\
  dipole moment (only working for water). Else: z-direction")
-parser.add_argument("-p", "--print",action="store_true",default=False, dest="print", 
+parser.add_argument("-p", "--print",action="store_true",default=False, dest="printing", 
                     help="Print out more information, for instance filename")
 args = parser.parse_args()
 
 
 for file in args.filename:
-    if args.print is True:
+    if args.printing is True:
         print file 
     try:
         finp = open(file,"r")
