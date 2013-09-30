9ed3e5eb7d4d3609f774b8afdb7bb40647e3a033
Arnfinn <arnfinn.steindal@uit.no>
2013-09-30 10:51:43 +0200

    more gitcommands

--- gitcommands.py
+++ gitcommands.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
 # print often used git commands
 
+print "SOME USEFUL COMMANDS"
 print "git reset --hard <hashtag>"
-print "git submodule update"
 print "git merge --no-ff <branchname>"
 print "git co -b <branchname> origin/<branchname>"
 print "git co -b Dalton2013_release origin/Dalton2013_release"
+print ""
+print "--making new branch--"
+print "git co -b branchname"
+print "--make it public--"
+print "git push -u origin branchname"
+print "-- git submodule --"
+print "git submodule init"
+print "git submodule update"
+print ""
