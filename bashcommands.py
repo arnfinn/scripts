310db46fea4d0a6e3c5303d5b97d56116136d7f3
Arnfinn <arnfinn.steindal@uit.no>
2014-02-06 14:09:36 +0100

    added new commands to bash print script

--- bashcommands.py
+++ bashcommands.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python
 # print bash commands
 
+print """
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxx Commands I often forget xxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+--- file without file ending ---
+for i in *.end; do echo ${i%\.*}; done
+
+--- list content in tar.gz file ---
+tar -ztvf file.tar.gz
+
+--- extract specific file ---
+tar --extract --file=file.tar.gz filename 
+
+--- add line after line ---
+sed '/patternstring/ a\
+new line string' file1
+
+--- number of lines in a directory ---
+ls -1 targetdir | wc -l
+"""
+
 print "find . -type f -print0 | xargs -0 sed -i -c 's/\-lt/\-ne/g'"
 print "sh -x 2013-05-28T*/*.check 2013-05-28T*/*.log"
-print "tar --extract --file=file.tar.gz filename"
-print "for i in *.end; do echo ${i%\.*}; done"
-print "--- number of lines in a directory ---"
-print "ls -1 targetdir | wc -l"
