cc912f0430be4a035e8bcf04a36234fae6c6c597
Arnfinn <arnfinn.steindal@uit.no>
2014-09-08 22:19:37 +0200

    print nm for ECD

--- get_output.py
+++ get_output.py
@@ -124,7 +124,7 @@ def get_ecd(lines):
                 return out
         if k > 9:
             r = i.split()
-            out += "{0} ({1} {2})  ".format(r[3],r[7],r[5])
+            out += "{0} ({1} {2})  ".format(int(1240.0/float(r[3])+0.5),r[7],r[5])
 
 def get_dipole(lines):
     count = False
